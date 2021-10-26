6234626e5124eecb9da625bc9b303fd0339c75ba
Rafael Marques da Silva <88897280+rafaelpbd@users.noreply.github.com>
2021-10-25 21:01:07 -0300

    Add files via upload
    Aulas do dia 25/10/21

---
+++ ex01_aula_25_10_21.js
@@ -0,0 +1,2 @@
+var alerta = "Atenção, Senha Incorreta";
+console.log(alerta);
---
+++ ex02_aula_25_10_21.js
@@ -0,0 +1,5 @@
+var nome = "Rafael";
+var vidas = 14;
+var municao = 76;
+
+console.log("\nPlayer: ",nome, "\nVidas: ", vidas , "\nMunição: ", municao,"\n");
