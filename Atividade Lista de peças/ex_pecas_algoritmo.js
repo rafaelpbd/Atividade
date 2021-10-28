acb6def9fa35a968c34483a5e485485ed0d752d4
unknown <rafaelpbd.90@gmail.com>
2021-10-27 22:03:40 -0300

    Update ex_pecas_algoritmo.js

--- Atividade Lista de peças/ex_pecas_algoritmo.js
+++ Atividade Lista de peças/ex_pecas_algoritmo.js
@@ -1,21 +1,24 @@
-const listadepecas = ['peças de teste', 'AB', 'peca B'];
+const listadepecas = ['Amortecedor', 'Filtro de óleo', 'Pastilha de freio', 'Radiador'];
+
 console.log("\nQuantidade de caracteres\n");
 if (listadepecas.length <= 10){
-    console.log("As peças podem ser cadastradas\n")
+    console.log("As peças podem ser cadastradas:\n")
+    console.log(listadepecas,"\n");
 };
 for(let val = 0; val < listadepecas.length; val++){
     pecaatual = listadepecas[val]
     if (pecaatual.length < 3){
-        console.log("Peça: "+ pecaatual + '- a peca possui nome inferior a 3 caracteres e nao pode ser cadastrada.')
+        console.log("Peça: "+ pecaatual + ' possui nome inferior a 3 caracteres e nao pode ser cadastrada.')
     }else{
-        console.log(pecaatual + ": a peça poder ser cadastrada")
+        console.log(pecaatual + " pode ser cadastrado")
     }
-console.log("\nPeso da peça\n")
-let pesodapeca = 50;
-    if (pesodapeca >= 100){
-        console.log("\nPeso suficiente\n")
-}else{
-    console.log('\nValor insuficiente\n')
-}
+    const pesodapeca = [50,75,105,225];
+    console.log("\nPeso da peça: ")
 
+    if (pesodapeca[val] >= 100){
+        console.log(pesodapeca[val]+"g - Peso suficiente\n")
+    }else{
+    console.log(pesodapeca[val]+"g - Valor insuficiente\n")
 }
+}
+console.log()
